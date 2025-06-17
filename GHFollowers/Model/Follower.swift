30e1503a71fead346fde8b97ac5e57cc0fdce19f
Luiz Paulo Prado <lp1717@gmail.com>
2025-06-17 15:02:12 -0300

    Create models

---
+++ GHFollowers/Model/Follower.swift
@@ -0,0 +1,13 @@
+//
+//  Follower.swift
+//  GHFollowers
+//
+//  Created by Luiz Paulo on 17/06/25.
+//
+
+import Foundation
+
+struct Follower: Codable {
+    var login: String
+    var avatarUrl: String
+}
